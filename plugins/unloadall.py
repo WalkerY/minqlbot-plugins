9e7f1c21dfeb46d0bd449469112522298981311a
WalkerY <WalkerY@users.noreply.github.com>
2015-05-29 12:33:22 +0200

    new comment

--- plugins/unloadall.py
+++ plugins/unloadall.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with minqlbot. If not, see <http://www.gnu.org/licenses/>.
 
+"""Command to load and unload all non-critical plugins at once.  """
 
 import minqlbot
 
